6188da5daefaef91ffa3b285c8079b84335c7758
shubham <tonygupta275@gmail.com>
2024-05-16 17:19:20 +0530

    add student list

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 import express from 'express';
+import studentList from './student.js';
 
-const app=express();
+const app = express();
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("hello")
-})
+});
+app.get("/getAllStudent", (req, res) => {
+    res.send(studentList);
+});
 
-app.listen(8000,()=>{
-    console.log("Server is running");
+app.get("/nameWithS", (req, res) => {
+    let filterStudnet = studentList.filter(student => student.name.startsWith('s') || student.name.startsWith('S'));
+    res.send(filterStudnet);
+})
+app.get("/meanStudents", (req, res) => {
+    let filterStudnet = studentList.filter(student => student.course=="mean");
+    res.send(filterStudnet);
+})
+app.get("/mernStudents", (req, res) => {
+    let filterStudnet = studentList.filter(student => student.course=="mern");
+    res.send(filterStudnet);
+})
+app.get("/phpStudents", (req, res) => {
+    let filterStudnet = studentList.filter(student => student.course=="php");
+    res.send(filterStudnet);
+})
+app.listen(8000, (err) => {
+    if (err) {
+        console.log("Server is not  running");
+    }
+    else {
+        console.log("Server is running");
+    }
 })
