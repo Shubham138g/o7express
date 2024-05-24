8757010ac2897ac59d65a5a41a298025005b1e88
shubham <tonygupta275@gmail.com>
2024-05-24 18:01:53 +0530

    add addUser route

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import studentList from './student.js';
 import product from './product.js';
 import Connection from './server/config/db.js';
 import userModel from './server/apis/user/userModel.js';
-import first from './fs.js';
+// import first from './fs.js';
 
 
 const app = express();
@@ -14,71 +14,92 @@ Connection();
 app.get("/", (req, res) => {
     res.send(`<h1 style='color:red' align='center'>Hello, Kaise ho aap?</h1>`);
 });
-app.get("/getAllStudent", (req, res) => {
-    res.send(studentList);
-});
-
-app.get("/nameWithS/:value", (req, res) => {
-    let filterStudnet = studentList.filter(student => student.name.startsWith(req.params.value));
-    res.send(filterStudnet);
-})
-app.get("/meanStudents", (req, res) => {
-    let filterStudnet = studentList.filter(student => student.course=="mean");
-    res.send(filterStudnet);
-})
-app.get("/mernStudents", (req, res) => {
-    let filterStudnet = studentList.filter(student => student.course=="mern");
-    res.send(filterStudnet);
-})
-app.get("/phpStudents", (req, res) => {
-    let filterStudnet = studentList.filter(student => student.course=="php");
-    res.send(filterStudnet);
-})
-
-app.get("/filterByQueryString",(req,res)=>{
-    let filterStudent=studentList.filter(student => student.course==req.query.course && student.name==req.query.name);
-    res.send(filterStudent);
-})
-app.get("/filterByParams/:course/:name",(req,res)=>{
-    let filterStudent=studentList.filter(student => student.course==req.params.course && student.name==req.params.name);
-    res.send(filterStudent);
-})
-
-
-
-
-
-app.get("/getAllProduct",(req,res)=>{
-    res.send(product)
-})
-
-app.get("/getProductByQuery",(req,res)=>{
-    // console.log(req.params);
-    let fiterProduct=product.filter(productCat=> productCat.category==req.query.category)
-    console.log(fiterProduct);
-    res.send(fiterProduct)
-})
-app.get("/getProductByParams/:price",(req,res)=>{
-    let fiterProduct=product.filter(productPrice=> productPrice.price<=req.params.price)
-    res.send(fiterProduct)
-})
-
-
-//23-05-2024
-app.post("/pass",(req,res)=>{
-    const email=req.body.email;
-    const pass=req.body.pass;
+app.post("/addUser",async (req, res) => {
+    console.log(User);
+    const total=await userModel.countDocuments()+1;
+    req.body.autoId=total;
+    const newUser=new userModel(User);
     try {
-        if(email=="admin@gmail.com" && pass=="123"){
-            res.json({status:200,message:"user login succesfully",success:true})
-        }
-        else{
-            res.json({status:404,message:`User not found`,success:false})
-        }
+      const data=  await newUser.save()
+        res.json({
+            status:200,
+            success:true,
+            message:"user created successfully",
+            data:data
+        })
     } catch (error) {
-        res.json({status:404,message:`error in pass route${error}`})
+        res.json({
+            status:500,
+            success:false,
+            message:"Error occour"+error
+        })
     }
-})
+});
+// app.get("/getAllStudent", (req, res) => {
+//     res.send(studentList);
+// });
+
+// app.get("/nameWithS/:value", (req, res) => {
+//     let filterStudnet = studentList.filter(student => student.name.startsWith(req.params.value));
+//     res.send(filterStudnet);
+// })
+// app.get("/meanStudents", (req, res) => {
+//     let filterStudnet = studentList.filter(student => student.course=="mean");
+//     res.send(filterStudnet);
+// })
+// app.get("/mernStudents", (req, res) => {
+//     let filterStudnet = studentList.filter(student => student.course=="mern");
+//     res.send(filterStudnet);
+// })
+// app.get("/phpStudents", (req, res) => {
+//     let filterStudnet = studentList.filter(student => student.course=="php");
+//     res.send(filterStudnet);
+// })
+
+// app.get("/filterByQueryString",(req,res)=>{
+//     let filterStudent=studentList.filter(student => student.course==req.query.course && student.name==req.query.name);
+//     res.send(filterStudent);
+// })
+// app.get("/filterByParams/:course/:name",(req,res)=>{
+//     let filterStudent=studentList.filter(student => student.course==req.params.course && student.name==req.params.name);
+//     res.send(filterStudent);
+// })
+
+
+
+
+
+// app.get("/getAllProduct",(req,res)=>{
+//     res.send(product)
+// })
+
+// app.get("/getProductByQuery",(req,res)=>{
+//     // console.log(req.params);
+//     let fiterProduct=product.filter(productCat=> productCat.category==req.query.category)
+//     console.log(fiterProduct);
+//     res.send(fiterProduct)
+// })
+// app.get("/getProductByParams/:price",(req,res)=>{
+//     let fiterProduct=product.filter(productPrice=> productPrice.price<=req.params.price)
+//     res.send(fiterProduct)
+// })
+
+
+// //23-05-2024
+// app.post("/pass",(req,res)=>{
+//     const email=req.body.email;
+//     const pass=req.body.pass;
+//     try {
+//         if(email=="admin@gmail.com" && pass=="123"){
+//             res.json({status:200,message:"user login succesfully",success:true})
+//         }
+//         else{
+//             res.json({status:404,message:`User not found`,success:false})
+//         }
+//     } catch (error) {
+//         res.json({status:404,message:`error in pass route${error}`})
+//     }
+// })
 
 //server handling
 app.listen(PORT, (err) => {
--- server/apis/user/userModel.js
+++ server/apis/user/userModel.js
@@ -1,6 +1,10 @@
 import mongoose from "mongoose";
 
 const UserSchema=new mongoose.Schema({
+    autoId:{
+        type:Number,
+        default:0
+    },
     name:{
         type:String,
         default:''
@@ -13,6 +17,14 @@ const UserSchema=new mongoose.Schema({
         type:String,
         default:''
     },
+    status:{
+        type:Boolean,
+        default:true
+    },
+    createdAt:{
+        type:Date,
+        default:Date.now()
+    }
 })
 
 const userModel=mongoose.model('user',UserSchema);
