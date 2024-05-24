85efaf74582be6631a742491af4b83bbf8af9681
shubham <tonygupta275@gmail.com>
2024-05-24 14:28:42 +0530

    add fs file

--- fs.js
+++ fs.js
@@ -5,16 +5,16 @@ const first =
     // fs.writeFile("first.txt","hello how are you",()=>{
     // console.log("file create successfully");
     // })
-//     fs.appendFile("first.txt", "\nmy name is ....", () => { });
-// fs.rename("first.text", "sec.txt", () => { });
-fs.unlink("sec.txt", () => {
-    console.log("file deleted succesfully");
-});
-const data=fs.readFile("first.txt","utf-8",(err,data)=>{
-    if(err){
-        console.log("error",err);
+    //     fs.appendFile("first.txt", "\nmy name is ....", () => { });
+    // fs.rename("first.text", "sec.txt", () => { });
+    fs.unlink("sec.txt", () => {
+        console.log("file deleted succesfully");
+    });
+const data = fs.readFile("first.txt", "utf-8", (err, data) => {
+    if (err) {
+        console.log("error", err);
     }
-    console.log("file content: \n",data);
+    console.log("file content: \n", data);
 })
 
 console.log(data);
